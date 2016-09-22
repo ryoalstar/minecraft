123ef0d767c34d01ec9568b0cc96f193c4bf7bee
Renual1337 <Rene.Roosen@renualsoftware.com>
2016-09-23 01:16:03 +0200

    Email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,14 +74,15 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: 'email-smtp.us-east-1.amazonaws.com',
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: 'minecraft-pe-servers.com',
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: 'AKIAJNI6YO2ZDTVT73ZA',
+    password: 'Avjw3uPJFPdtQZwCikva8pgVFz/fkN4H/APHwmdlOx5I'
   }
+  
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
