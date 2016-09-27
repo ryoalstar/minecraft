a8c3eaa0369d284a91716307d1c413f09648b29c
Renual <Rene.Roosen@renualsoftware.com>
2016-09-27 21:00:25 +0200

    d

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,4 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-
-  ActsAsTaggableOn.force_binary_collation = true
-
 end
