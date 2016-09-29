48969e1ff45c2e5454f3a77869edfcfe8cc162e6
Simon <simonmail@zoho.com>
2016-09-29 04:44:46 +0200

    Fix word wrap issue

--- db/migrate/20160807215244_devise_create_users.rb
+++ db/migrate/20160807215244_devise_create_users.rb
@@ -1,6 +1,6 @@
-class AddDeviseToUsers < ActiveRecord::Migration
-  def self.up
-    change_table :users do |t|
+class DeviseCreateUsers < ActiveRecord::Migration
+  def change
+    create_table(:users) do |t|
 
       ## Recoverable
       t.string   :reset_password_token
@@ -35,11 +35,5 @@ class AddDeviseToUsers < ActiveRecord::Migration
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
-  end
-
-  def self.down
-    # By default, we don't want to make any assumption about how to roll back a migration when your
-    # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
   end
 end
