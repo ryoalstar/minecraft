d825a15481d5f6554b55339ce03b172f7d0d8064
Simon <simonmail@zoho.com>
2016-09-29 08:40:15 +0200

    Fix zoom, collapse

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,5 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w(jquery-1.11.1.min.js)
+Rails.application.config.assets.precompile += %w(jquery-1.11.1.min.js1)
 Rails.application.config.assets.precompile += %w(bootstrap.js)
